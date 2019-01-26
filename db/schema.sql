e6a990f2b2ea650576492e7f837acaffda2ca794
Nathan <nathanlserrano@gmail.com>
2019-01-25 16:23:12 -0800

    Added in SQL files for database

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 create database burgers_db;
 use burgers_db;
 
+
 create table burgers (
-	item_id integer not null auto_increment,
-    product_name varchar(50) not null,
-    department_name varchar(50) not null,
-    price integer default 0,
-    stock_quantity integer default 0,
-    primary key(item_id)
+	id integer not null auto_increment,
+    burger_name varchar(50),
+    devoured boolean,
+    primary key(id)
 );
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+Insert into burgers (burger_name, devoured)
+values 
+	("Chicken Burger", false),
+	("Charbroiled Burger", false);
