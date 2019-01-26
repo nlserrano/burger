c8a8c713afe25c2f528146b6bde2d9333f7b2e9e
Nathan <nathanlserrano@gmail.com>
2019-01-26 14:30:17 -0800

    Setting up JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "teststuff",
-    database: "burgers_db"
+    host: "uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "yc05q4lv3sywu915",
+    password: "crzr50ako6ht8rho",
+    database: "tauq21xbtowjposn"
   });
 };
 
