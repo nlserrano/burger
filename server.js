ce97a3c34f234618ade3e3972f235f85c2343153
Nathan <nathanlserrano@gmail.com>
2019-01-26 02:02:19 -0800

    Defined port for listening within server.js

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
+//Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+
+//Define port the server will be listening on.
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+//Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + '/public'));
+
+//Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Parse application/json
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+//App is listening...
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
