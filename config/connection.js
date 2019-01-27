01d88226d1bc2968b12dc2e2e84ea4f0e9d44e35
Nathan <nathanlserrano@gmail.com>
2019-01-26 23:08:07 -0800

    Finished styling

--- config/connection.js
+++ config/connection.js
@@ -15,25 +15,3 @@ if (process.env.JAWSDB_URL) {
 
 connection.connect();
 module.exports = connection;
-
-
-// // Set up our connection information
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "teststuff",
-//   database: "burgers_db"
-// });
-
-// // Connect to the database
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection
-// module.exports = connection;
